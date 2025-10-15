69c8e11d50a277bbee93e5f7f8a48a496209748c
LuLaValva <lukelavalva@gmail.com>
2025-10-15 10:21:51 -0700

    fix: modernize application and remove the most obvious anti-patterns

--- .storybook/preview.ts
+++
@@ -1,6 +0,0 @@
-import type { Parameters } from "@storybook/marko";
-import "../src/pages/+layout.style.css";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-} satisfies Parameters;
